f63a76bb22bb1ec7af5296e66ced01b2be82e06d
Chris Adams <uqcadams@gmail.com>
2021-04-21 17:07:24 +1000

    Finished core implementation
    Still need to remove the console logs used during testing

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 // DOM selectors //
+  // selects the onscreen keyboard section
 const qwerty = document.querySelector(`#qwerty`);
+  // selects the container for holding and displaying game phrase
 const phrase = document.querySelector(`#phrase`);
+  // selects the start button to hide overlay
 const startGameButton = document.querySelector('.btn__reset');
+  // selects the game overlay
+const overlay = document.querySelector('#overlay');
+
 
 // Variables 
+  // stores the number of "missed" guesses 
 let missed = 0;
 
 // Arrays
+  // stores array of phrases to parse into game
 const phrases = [
     'chris',
-    'julia',
-    'rine',
-    'laura',
-    'chelsea'
+    'test',
+    'one',
+    'okay',
+    'four'
 ];
-
-
 
 // -- turns the display overlay off to reveal the game board
 startGameButton.addEventListener('click', () => {
     startGameButton.parentElement.style.display = 'none';
 })
 
-
 // Functions 
-// -- creates an empty array, generates a random integer based on the length of the input array, uses that number to select a value (as an index). Then splits the selected value into a new array, and returns the new phrase array.
+  // -- creates an empty array, generates a random integer based on the length of the input array, uses that number to select a value (as an index). Then splits the selected value into a new array, and returns the new phrase array.
 getRandomPhraseAsArray = (arr) => {
   let randomPhrase = [];
   let randomNumber = Math.floor(Math.random() * arr.length);
@@ -33,7 +38,7 @@ getRandomPhraseAsArray = (arr) => {
   return randomPhrase;
 }
 
-// -- takes an array and assigns the "letter" class to the values that are letters, and the "space" class to the spaces.
+  // -- takes an array and assigns the "letter" class to the values that are letters, and the "space" class to the spaces. Then, it appends them to the phrase section for display on the screen.
 addPhraseToDisplay = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     let ul = phrase.firstElementChild;
@@ -50,76 +55,63 @@ addPhraseToDisplay = (arr) => {
 };
 
 
-//// startGameButton.addEventListener('click', () => {})
-//// const getRandomPhraseAsArray = arr => {}
-//// const addPhraseToDisplay = arr => {}
-//// const checkLetter = arr => {}
-// qwerty.addEventListener('click', e => {})
-// const checkWin = () => {}
-
-
-// @@@ delete this later @@@ pre-places phrase on page
-const phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
-
-
+// -- function to compare text content of the button click to elements within the phrase array
 checkLetter = (buttonTarget) => {
+  // creates a checkLetter array with all li elements in the displayed phrase
     let checkLetter = document.querySelectorAll('#phrase li');
-    let match = false;
-
+    let match = null;
+  // loops through checkLetter array
     for (let i = 0; i < checkLetter.length; i++) {
         console.log(checkLetter[i].textContent);
+  // if text of button target is in checkLetter array, add show class to it
         if (buttonTarget.textContent === checkLetter[i].textContent) {
             checkLetter[i].className += " show";
-            return true;
-            break;
+            match = checkLetter[i].textContent;
         }
     }
+    return match;   
 }
 
-
-// qwerty.addEventListener('click', e => {})
-
+// -- adds a click event to the qwerty element
 qwerty.addEventListener("click", (e) => {
   console.log("qwerty event listener activated");
 
   let clickedButton = e.target;
 
+  // if the target of the click is a button...
   if (clickedButton.tagName === "BUTTON") {
+    // ... then add the chosen class to it
     clickedButton.className += " chosen";
-    checkLetter(clickedButton);
+    // ... then run the checkLetter function on that button 
+    let match = checkLetter(clickedButton);
+    // ... then disable the button to prevent further clips
     clickedButton.disabled = true;
+    // ... then see if it found a letter
+    if (match === null) {
+      console.log('no letter was found');
+      missed++;
+      console.log(missed);
+        // selects the heart elements
+      let heart = document.querySelector('#scoreboard ol').firstElementChild;
+      heart.remove();
+    }
   }
+  checkWin();
 });
 
-// checkLetter = (buttonTarget) => {
-//     console.log('check letter function activated');
-//     console.log(`buttonTarget.textContent is ${buttonTarget.textContent}`);
-//     let letterElements = document.querySelectorAll('.letter');
-//     let letterFound = ['hi'];
-//     for ( let i = 0; i < letterElements.length; i++ ) {
-//         if ( buttonTarget.textContent === letterElements[i].textContent ) {
-//             letterElements[i].className += " show";
-//             // letterFound += letterFound.push(letterElements[i].textContent);
-//             letterFound += letterFound.push(`${letterElements[i].textContent}`);
-//             console.log(letterFound);
-//             console.log('added class');
+checkWin = () => {
+  let letters = document.getElementsByClassName('letter');
+  let shown = document.getElementsByClassName('show');
+  if (letters.length === shown.length) {
+    overlay.className += ' win';
+    overlay.firstElementChild.textContent = "WINNER!!!"
+    overlay.style.display = 'flex';
+  } else if (missed > 4) {
+    overlay.className += ' lose';
+    overlay.firstElementChild.textContent = "YOU HAVE FAILED!!!"
+    overlay.style.display = 'flex';
+  }
+}
 
-//         } 
-//         else {
-//             missed ++;
-//             console.log(`Missed = ${missed}`);
-//             // return null;
-//         }
-//     }
-// }
-
-
-
-// -- Test to log the target button to the console
-// testFunction = (buttonTarget) => {
-//     console.log(buttonTarget.textContent);
-// }
-
-
-
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
